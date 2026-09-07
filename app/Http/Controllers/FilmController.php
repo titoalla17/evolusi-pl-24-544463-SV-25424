d273d57cb10d0b8e815e7f6fd967dc72da77819f
titoalla17 <titoallakhairi@mail.ugm.ac.id>
2026-09-07 20:22:20 +0700

    feat: setup controller and routes for movie search

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\FilmApiService;
 
 class FilmController extends Controller
 {
-    //
+    protected $filmApiService;
+
+    public function __construct(FilmApiService $filmApiService)
+    {
+        $this->filmApiService = $filmApiService;
+    }
+
+    public function index()
+    {
+        return view('films.index', ['hasil' => null]);
+    }
+
+    public function cari(Request $request)
+    {
+        $request->validate(['judul' => 'required|string|min:2']);
+        $hasil = $this->filmApiService->cariFilm($request->judul);
+        return view('films.index', compact('hasil'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FilmController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FilmController::class, 'index'])->name('films.index');
+Route::get('/cari', [FilmController::class, 'cari'])->name('films.cari');
